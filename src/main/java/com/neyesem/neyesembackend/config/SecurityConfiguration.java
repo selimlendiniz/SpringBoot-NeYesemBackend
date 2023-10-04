98461dbe82fb36f54275fb6b7abfffdf95562e87
selimlendiniz <sardacevik@icloud.com>
2023-10-05 00:05:11 +0300

    Basic Security Added to Test Restaurant CRUD

---
+++ src/main/java/com/neyesem/neyesembackend/config/SecurityConfiguration.java
@@ -0,0 +1,29 @@
+package com.neyesem.neyesembackend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf()
+                .disable()
+                .authorizeHttpRequests()
+                .anyRequest()
+                .permitAll()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        return http.build();
+    }
+}

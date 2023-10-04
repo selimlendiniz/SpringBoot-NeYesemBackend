2b6c06c546975cab30b21eb46a189fce28f82d7a
selimlendiniz <sardacevik@icloud.com>
2023-10-04 20:59:14 +0300

    initial commit

---
+++ src/main/java/com/neyesem/neyesembackend/NeyesembackendApplication.java
@@ -0,0 +1,13 @@
+package com.neyesem.neyesembackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeyesembackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeyesembackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/neyesem/neyesembackend/NeyesembackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neyesem.neyesembackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeyesembackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
